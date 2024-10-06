6ad494d4888d5d6dc5f5d81017e0cfffb1274f5f
MaLoskins <matthew.haskins.mh@gmail.com>
2024-10-06 21:38:29 +0800

    Remove K-Anonynimity Tab (integrated elsewhere)

--- src/binning/__init__.py
+++ src/binning/__init__.py
@@ -3,5 +3,4 @@
 from .data_binner import DataBinner
 from .data_integrity_assessor import DataIntegrityAssessor
 from .unique_bin_identifier import UniqueBinIdentifier
-from .k_anonymity_binner import KAnonymityBinner  
 
